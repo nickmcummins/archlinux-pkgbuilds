9d3d60e08abe2d522cb0fd71462cc117a4291730
Nick M Cummins <nickmcummins@gmail.com>
2025-05-30 15:58:15 -0700

    stellarium-git: version 25.1.r32483.7da2825000

--- read_cmake_version.py
+++ read_cmake_version.py
@@ -4,15 +4,19 @@ import re
 cmake_filename = sys.argv[1]        # e.g. CMake/vtkVersion.cmake
 version_major_var = sys.argv[2]     # e.g. VTK_MAJOR_VERSION
 version_minor_var = sys.argv[3]     # e.g. VTK_MINOR_VERSION
-version_patch_var = sys.argv[4]     # e.g. VTK_BUILD_VERSION
+if len(sys.argv) > 4:
+    version_patch_var = sys.argv[4]     # e.g. VTK_BUILD_VERSION
 
-cmake_vars_regex = r'(set\s*\()([^\s]+)[\s"]*([^)^"]+)'
+cmake_vars_regex = r'(set|SET\s*\()([^\s]+)[\s"]*([^)^"]+)'
 
 with open(cmake_filename, 'r') as cmake_file:
-    vars = dict(list(map(lambda varline: (re.search(cmake_vars_regex, varline).group(2), re.search(cmake_vars_regex, varline).group(3)), filter(lambda line: line.startswith('set'), cmake_file.read().split('\n')))))
+    vars = dict(list(map(lambda varline: (re.search(cmake_vars_regex, varline).group(2), re.search(cmake_vars_regex, varline).group(3)), filter(lambda line: line.lower().startswith('set') and re.search(cmake_vars_regex, line) is not None, cmake_file.read().split('\n')))))
 
     major_version = vars[version_major_var]
     minor_version = vars[version_minor_var]
-    build_version = vars[version_patch_var]
+    if len(sys.argv) == 4:
+        print(f'{major_version}.{minor_version}')
 
-    print(f'{major_version}.{minor_version}.{build_version}')
+    if len(sys.argv) > 4:
+        build_version = vars[version_patch_var]
+        print(f'{major_version}.{minor_version}.{build_version}')
