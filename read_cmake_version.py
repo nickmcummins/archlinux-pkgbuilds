f49dccce89d17d8ed96f40953399092e147c3ccc
Nick M Cummins <nickmcummins@gmail.com>
2025-05-31 18:36:02 -0700

    read_cmake_version.py - support arbitrary number of version vars as well as ability to get version defined in PROJECT()

--- read_cmake_version.py
+++ read_cmake_version.py
@@ -1,22 +1,45 @@
+# Usage:
+#   read_cmake_version.py CMakeLists.txt STELLARIUM_MAJOR STELLARIUM_MINOR
+#   read_cmake_version.py CMakeLists.txt project
+
 import sys
 import re
 
-cmake_filename = sys.argv[1]        # e.g. CMake/vtkVersion.cmake
-version_major_var = sys.argv[2]     # e.g. VTK_MAJOR_VERSION
-version_minor_var = sys.argv[3]     # e.g. VTK_MINOR_VERSION
-if len(sys.argv) > 4:
-    version_patch_var = sys.argv[4]     # e.g. VTK_BUILD_VERSION
+cmake_vars_regex = r'([set|SET]\s*\()([^\s]+)[\s"]*([^)^"]+)'
+cmake_project_definition_regex = r'(project\s*\()([^\s]+)\sVERSION\s+([^)^"]+)'
 
-cmake_vars_regex = r'(set|SET\s*\()([^\s]+)[\s"]*([^)^"]+)'
+def read_project_version():
+    with open(cmake_filename, 'r') as cmake_file:
+        project_def = list(filter(lambda line: line.lower().startswith('project') and re.search(cmake_project_definition_regex, line) is not None, cmake_file.read().split('\n')))[0]
+        project_version = re.search(cmake_project_definition_regex, project_def).group(3)
+        print(project_version)
 
-with open(cmake_filename, 'r') as cmake_file:
-    vars = dict(list(map(lambda varline: (re.search(cmake_vars_regex, varline).group(2), re.search(cmake_vars_regex, varline).group(3)), filter(lambda line: line.lower().startswith('set') and re.search(cmake_vars_regex, line) is not None, cmake_file.read().split('\n')))))
+def read_from_cmake_variables():
+    try:
+        version_vars = []
+        argindex = 2
+        while argindex < len(sys.argv):
+            version_vars.append(sys.argv[argindex])
+            argindex += 1
 
-    major_version = vars[version_major_var]
-    minor_version = vars[version_minor_var]
-    if len(sys.argv) == 4:
-        print(f'{major_version}.{minor_version}')
+        with open(cmake_filename, 'r') as cmake_file:
+            vars = dict(list(map(lambda varline: (re.search(cmake_vars_regex, varline).group(2), re.search(cmake_vars_regex, varline).group(3)), filter(lambda line: line.lower().startswith('set') and re.search(cmake_vars_regex, line) is not None, cmake_file.read().split('\n')))))
 
-    if len(sys.argv) > 4:
-        build_version = vars[version_patch_var]
-        print(f'{major_version}.{minor_version}.{build_version}')
+            version = []
+            for version_var in version_vars:
+                version.append(vars[version_var])
+
+            print('.'.join(version))
+
+    except Exception as e:
+        print(f'Error trying to read version variables {version_vars} from cmake file {cmake_filename}:\n{e}.\nVariables found:')
+        for var in vars.keys():
+            value = vars[var]
+            print(f'\t{var}={value}')
+
+if __name__ == '__main__':
+    cmake_filename = sys.argv[1]        # e.g. CMake/vtkVersion.cmake
+    if sys.argv[2] == 'project':
+        read_project_version()
+    else:
+        read_from_cmake_variables()
